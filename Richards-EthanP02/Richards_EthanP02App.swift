4b67a0bd6fe186e1901ebf16a90a070f10cb85f7
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2024-03-02 20:50:46 -0700

    Initial Commit

---
+++ Richards-EthanP02/Richards_EthanP02App.swift
@@ -0,0 +1,17 @@
+//
+//  Richards_EthanP02App.swift
+//  Richards-EthanP02
+//
+//  Created by Ethan Richards on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Richards_EthanP02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
