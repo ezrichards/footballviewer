8c4c03d197ddd9db6ef41dcd72128278bc31dea9
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2024-04-30 21:59:32 -0600

    playerview: add searching

--- footballviewer/Views/PlayerView.swift
+++ footballviewer/Views/PlayerView.swift
@@ -12,16 +12,33 @@ struct PlayerView: View {
     
     var players: [PlayerResponse]
     
+    @State private var searchText = ""
+    
+    var filteredPlayers: [PlayerResponse] {
+        guard !searchText.isEmpty else { return players }
+        
+        var filteredPlayers: [PlayerResponse] = []
+        for player in players {
+            if let name = player.player.name {
+                if name.lowercased().contains(searchText.lowercased()) {
+                    filteredPlayers.append(player)
+                }
+            }
+        }
+        return filteredPlayers
+    }
+    
     @Binding var selectedPlayers: Set<PlayerResponse.ID>
     
     var body: some View {
         List(selection: $selectedPlayers) {
             Section(header: Text("Players")) {
-                ForEach(players, id: \.id) { player in
+                ForEach(filteredPlayers, id: \.id) { player in
                     Text(player.player.name ?? "")
                 }
             }
         }
+        .searchable(text: $searchText, prompt: "Search players..")
         .padding()
         .frame(maxHeight: 300)
     }
