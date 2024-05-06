4afd1f65787943f761f69a9b69bb0e8c11e37b0f
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2024-05-06 10:35:34 -0600

    fix detailview bug

--- footballviewer/Views/DetailView.swift
+++ footballviewer/Views/DetailView.swift
@@ -16,44 +16,52 @@ struct DetailView: View {
     
     var body: some View {
         if let playerOne = player, let response = playerOne.response?.first {
-
-            if let photo = response.player.photo {
-                AsyncImage(url: URL(string: photo)) { image in
-                    image
-                } placeholder: {
-                    ProgressView()
+            VStack {
+                if let photo = response.player.photo {
+                    AsyncImage(url: URL(string: photo)) { image in
+                        image
+                    } placeholder: {
+                        ProgressView()
+                    }
                 }
-            }
-            
-            ScrollView {
-                VStack {
+
+                ScrollView {
                     Text("\(response.player.name)").bold()
                     Text("Age: \(response.player.age)")
                     Text("Nationality: \(response.player.nationality ?? "")")
+                    
+                    Spacer()
+                    
+                    let statistic = response.statistics.first(where: { viewModel.leagueSelection.contains($0.league?.id) })
+                    
+                    Text("General Statistics").bold()
+                    Text("Appearances: \(statistic?.games.appearences ?? 0)")
+                    
+                    let rating = String(format: "%.2f", Float(statistic?.games.rating ?? "N/A") ?? 0.0)
+                    
+                    Text("Average Rating: \(rating)")
+                    Text("Position: \(statistic?.games.position ?? "N/A")")
+                    
+                    Spacer()
+                    
+                    Text("Goals/Assists").bold()
+                    Text("Goals: \(statistic?.goals.total ?? 0)")
+                    Text("Assists: \(statistic?.goals.assists ?? 0)")
+                    
+                    Spacer()
+                    
+                    Text("Passes").bold()
+                    Text("Total: \(statistic?.passes.total ?? 0)")
+                    Text("Key Passes: \(statistic?.passes.key ?? 0)")
+                    Text("Accuracy: \(statistic?.passes.accuracy ?? 0)%")
+                    
+                    Spacer()
+                    
+                    Text("Defensive Actions").bold()
+                    Text("Tackles: \(statistic?.tackles.total ?? 0)")
+                    Text("Blocks: \(statistic?.tackles.blocks ?? 0)")
+                    Text("Interceptions: \(statistic?.tackles.interceptions ?? 0)")
                 }
-                
-                Spacer()
-                
-                ForEach(response.statistics) { statistic in
-                    if viewModel.leagueSelection.contains(statistic.league?.id) {
-                            Text("General Statistics").bold()
-                            Text("Appearances: \(statistic.games.appearences)")
-                            Text("Average Rating: \(statistic.games.rating)")
-                            Text("Position: \(statistic.games.position)")
-
-                            Spacer()
-                            
-                            Text("Goals/Assists").bold()
-                            Text("Goals: \(statistic.goals.total)")
-                            Text("Assists: \(statistic.goals.assists)")
-                            Spacer()
-                            
-                            Text("Passes").bold()
-                            Text("Total: \(statistic.passes.total)")
-                            Text("Key Passes: \(statistic.passes.key)")
-                            Text("Accuracy: \(statistic.passes.accuracy)%")
-                        }
-                    }
             }
         } else {
             Text("No player selected!")
